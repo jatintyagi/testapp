ac150fa222516ba1c4431ba681ccfcf525454c3e
jatintyagi <jatintyagi86@gmail.com>
2022-03-27 19:57:02 +0530

    Create utilitie.js

---
+++ src/shared/utilitie.js
@@ -0,0 +1,28 @@
+export const sortData = (prop) => {
+    return function(a, b) {    
+        if (a[prop] > b[prop]) {    
+            return 1;    
+        } else if (a[prop] < b[prop]) {    
+            return -1;    
+        }    
+        return 0;    
+    }  
+};
+
+export const isJson = (item) => {
+    item = typeof item !== "string"
+        ? JSON.stringify(item)
+        : item;
+
+    try {
+        item = JSON.parse(item);
+    } catch (e) {
+        return false;
+    }
+
+    if (typeof item === "object" && item !== null) {
+        return true;
+    }
+
+    return false;
+}
